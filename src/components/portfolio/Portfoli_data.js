2c87fbb76d84755e746394f8e16d6e656ed24616
bobby khadka <khadkaasmith2059@gmail.com>
2023-04-09 10:28:30 +0545

    reorder recent work

--- src/components/portfolio/Portfoli_data.js
+++ src/components/portfolio/Portfoli_data.js
@@ -1,21 +1,9 @@
 const Data = [
   {
-    img: "portfolio1.jpg",
-    title: "Ecommerce website",
-    link1: "https://github.com/khadkabobby/Ecommerce-website",
-    link2: "https://eccomerceprojects.netlify.app/",
-  },
-  {
-    img: "portfolio2.jpg",
-    title: "University website",
-    link1: "https://github.com/khadkabobby/University-website",
-    link2: "https://universitybobby.netlify.app/contact.html",
-  },
-  {
-    img: "portfolio3.jpg",
-    title: "Portfolio website version 1",
-    link1: "https://github.com/khadkabobby/Portfolio-version1",
-    link2: "https://bobbykhadka39.netlify.app/",
+    img: "portfolio5.png",
+    title: "Movix App",
+    link1: "https://github.com/khadkabobby/Movix-web-app",
+    link2: "https://movix-bobby.netlify.app/",
   },
   {
     img: "portfolio4.png",
@@ -24,10 +12,22 @@ const Data = [
     link2: "https://bobbydzign.netlify.app/",
   },
   {
-    img: "portfolio5.png",
-    title: "Movix App",
-    link1: "https://github.com/khadkabobby/Movix-web-app",
-    link2: "https://movix-bobby.netlify.app/",
+    img: "portfolio3.jpg",
+    title: "Portfolio website version 1",
+    link1: "https://github.com/khadkabobby/Portfolio-version1",
+    link2: "https://bobbykhadka39.netlify.app/",
+  },
+  {
+    img: "portfolio2.jpg",
+    title: "University website",
+    link1: "https://github.com/khadkabobby/University-website",
+    link2: "https://universitybobby.netlify.app/contact.html",
+  },
+  {
+    img: "portfolio1.jpg",
+    title: "Ecommerce website",
+    link1: "https://github.com/khadkabobby/Ecommerce-website",
+    link2: "https://eccomerceprojects.netlify.app/",
   },
 ];
 export default Data;
